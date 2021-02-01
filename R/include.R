cb2d4ae3c098ed4d04f73ce64758cb02ce20e02b
Nathan Hattersley <nate.hattersley@gmail.com>
2021-02-01 09:42:36 -0600

    more packages; remove my own read_csv implementation in favor of vroom everywhere

--- R/include.R
+++ R/include.R
@@ -1,19 +1,23 @@
 library(ggmap)
+library(gganimate)
+library(gifski)
+library(jsonlite)
 library(tidyverse)
 library(janitor)
 library(magrittr)
+library(glue)
+library(lubridate)
 library(crul)
 library(vroom)
+library(sf)
+library(transformr)
 
 # register API key for ggmap
 register_google(key = read_file("api_secret.txt"))
 
-# read csv implementation of my choosing, with option to suppress output of column type guessing
-csv <- function(file, suppress = F, ...) if (suppress) suppressMessages(vroom(file, ...)) else vroom(file, ...)
-
 # compress any CSV files I get so that I save storage space :)
-compress_csv <- function(file) {
-  file <- paste0("data/",file,".csv")
-  vroom(file) %>% vroom_write(paste0(file,".gz"))
+.compress_csv <- function(file) {
+  file <- glue("data/{file}.csv")
+  vroom(file) %>% vroom_write(glue("{file}.gz"))
   file.remove(file)
 }
