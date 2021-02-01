47d441a535984571816b2a77c8cb63cff6243e95
Nathan Hattersley <nate.hattersley@gmail.com>
2021-02-01 09:43:56 -0600

    initial plots of citations by census tract over time

---
+++ R/graph_citations_by_tract.R
@@ -0,0 +1,99 @@
+# Reads in the citations by tract CSV and compiles
+# 1. Static per-month plots of citations by tract
+# 2. GIF animating the per-month plots
+
+source("R/include.R", local = TRUE) # read the include file
+
+
+
+##### Cleaning and Shaping #####
+
+# This shapefile has a different coordinate system, and I really just
+# want the convex hull so I can intersect the statewide census tract
+# file to get the Austin-area census tracts
+city_boundaries <-
+  read_sf("data/austin_city_limits.shp") %>%
+  st_transform("NAD83") %>%
+  st_convex_hull
+
+census_tracts <-
+  read_sf("data/texas_census_tracts.shp") %>%
+  st_intersection(city_boundaries)
+
+# Take our citation by tract data and
+# 1. add the "group_date" which is the granularity we want to aggregate by
+# 2. group and summarise number of citations by census tract per aggregation period
+# 3. ungroup and run complete to ensure that every tract has a datum for every period
+# 4. join on census tract to attach the sf geometry to our dataset
+# 5. select only the columns we care about
+citations_by_tract <- vroom("data/citations_by_tract.csv.gz") %>%
+  mutate(group_date = floor_date(date, "month")) %>%
+  group_by(census_tract, group_date) %>%
+  summarise(citations = n())  %>%
+  ungroup %>%
+  complete(census_tract, group_date, fill = list(citations = 0)) %>%
+  inner_join(census_tracts, c("census_tract" = "TRACTCE")) %>%
+  select(citations, census_tract, group_date, geometry)
+
+
+
+##### Static Plot #####
+
+# set limits for the fill scale so that colors represent the same thing for every plot
+.lims <- with(citations_by_tract, c(min(citations), max(citations)))
+lims(fill = .lims)
+
+# function to plot a static subset of the data
+plot_citations <- function(d)
+  ggplot(d) +
+  geom_sf(aes(
+    fill = citations,
+    geometry = geometry,
+    group = census_tract
+  )) +
+  scale_fill_gradient(
+    low = "white",
+    high = "red",
+    name = "Citations per month",
+    guide = FALSE,
+    trans = "log1p"
+  ) +
+  theme_minimal()
+
+# For each aggregation period, make a static plot
+citations_by_tract %>%
+  group_by(group_date) %>%
+  group_walk(function(group_data, group_key) {
+    date <- group_key$group_date
+    plot_citations(group_data) + ggtitle(glue("Anti-Homeless Citations, {format(date, '%B %Y')}"))
+    ggsave(glue("figures/citations_by_tract_{format(date, '%Y_%m')}.png"),
+           device = "png")
+  })
+
+# Zip those static plots to save space. Note tar = "tar" b/c Sys.getenv("tar") was blank for me
+Sys.glob("figures/citations_by_tract_*.png") %T>%
+  tar(
+    tarfile = "figures/citations_by_tract.tar.gz",
+    files = .,
+    compression = "gzip",
+    tar = "tar"
+  ) %T>%
+  file.remove
+
+
+##### Animated Plot #####
+
+# let's also make an animated plot with gganimate
+gif_plot <- citations_by_tract %>% plot_citations +
+  transition_time(group_date) +
+  labs(title = '{format(frame_time, "%B %Y")}') +
+  enter_appear() +
+  exit_disappear()
+
+# ~50 months with 30 seconds total duration = ~half a second per month
+animate(gif_plot,
+        renderer = gifski_renderer(),
+        duration = 30,
+        fps = 5)
+
+anim_save("figures/citations_by_tract.gif")
