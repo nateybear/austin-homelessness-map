891d6d06efb74682501bed289b9580f98bfb691f
Nathan Hattersley <nate.hattersley@gmail.com>
2021-04-21 11:28:52 -0500

    compile simple graph of citations over time

---
+++ R/describe/graph_citations_over_time.R
@@ -0,0 +1,11 @@
+citations <- vroom(here("data/citations_by_tract.csv.gz")) %>% aggregate_event_data(tract_column = offense_description)
+
+ggplot(citations, aes(x = group_date, y = citations)) + 
+  geom_line(color = muted("blue")) + 
+  facet_wrap(~offense_description, nrow = 1) +
+  geom_vline(xintercept = as_datetime(ymd(20180628)), color = muted("red")) +
+  geom_vline(xintercept = as_datetime(ymd(20190629)), color = muted("green"))
+  labs(y = "Citations per Month", x = "") +
+  theme_calc()
+
+ggsave(here("figures/citations_over_time.png"))
